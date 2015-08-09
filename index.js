b1639a60cbe7d0a690c9c3b9dbb261e3b9494c4f
Tobias Reich <tobias.reich.ich@gmail.com>
2015-08-09 17:58:17 +0200

    Handle sub-modules
    => Start setup when config missing
    => Start server when config available

--- index.js
+++ index.js
@@ -3,3 +3,19 @@
  * @author    Tobias Reich
  * @copyright 2015 by Tobias Reich
  */
+
+var core   = require('./core/index.js'),
+    setup  = require('./setup/index.js'),
+    path   = require('path')
+
+var keyPath    = path.join(__dirname, 'setup/data/key.pem'),
+    certPath   = path.join(__dirname, 'setup/data/cert.pem'),
+    configPath = path.join(__dirname, 'setup/data/config.json')
+
+// Configuration missing? => Start the setup
+if (setup.exists(configPath)===false) {
+	setup.start(configPath)
+	return true
+}
+
+core(keyPath, certPath, configPath)
