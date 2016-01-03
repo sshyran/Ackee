466c7745fdf669452b4460ffd91f412c4651b288
Tobias Reich <tobias.reich.ich@gmail.com>
2016-01-03 17:01:08 +0100

    Updated index file with some ES2015 syntax

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
+'use strict'
+
 /*
  * @name      Ackee
  * @author    Tobias Reich
  * @copyright 2015 by Tobias Reich
  */
 
-var server = require('./server/index.js'),
-    setup  = require('./setup/index.js'),
-    path   = require('path')
+let path   = require('path'),
+    server = require('./server/index.js'),
+    setup  = require('./setup/index.js')
 
-var httpPort  = process.env.HTTP_PORT,
+let httpPort  = process.env.HTTP_PORT,
     httpsPort = process.env.HTTPS_PORT
 
-var keyPath     = process.env.KEY_PATH || path.join(__dirname, 'data/key.pem'),
+let keyPath     = process.env.KEY_PATH || path.join(__dirname, 'data/key.pem'),
     certPath    = process.env.CERT_PATH || path.join(__dirname, 'data/cert.pem'),
     configPath  = process.env.CONFIG_PATH || path.join(__dirname, 'data/config.json'),
     pluginsPath = process.env.PLUGINS_PATH || path.join(__dirname, 'node_modules/')
 
-var start = function() {
+const start = function() {
 
 	// Start Ackee
 	server.start(httpPort, httpsPort, keyPath, certPath, configPath, pluginsPath)
 
 }
 
-setup.exists(configPath, function(exists) {
+setup.exists(configPath, (exists) => {
 
 	// Configuration missing => Start the setup
 	// Configuration exists => Start Ackee
